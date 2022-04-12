e4530f258bf57cec4525ba9e70bb1350b523ad3a
shutkin.90 <shutkin.90@mail.ru>
2022-04-12 22:04:41 +0300

    Initial Commit

---
+++ ActivitySuggestions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActivitySuggestions
+//
+//  Created by Aleksandr on 12.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
